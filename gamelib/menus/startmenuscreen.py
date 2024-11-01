ebffb53bae42608831461bdc26c4b34cd39a9b41
romibi <romibi@bluewin.ch>
2024-11-01 23:55:34 +0100

    Add proper key handling routine to start screen

--- gamelib/menus/startmenuscreen.py
+++ gamelib/menus/startmenuscreen.py
@@ -1,7 +1,8 @@
 import pygame as pg
 from pygame import Rect
 
-from gamelib.data_helper_functions import load_image
+from config.buttonconfig import SERIAL_BUTTON_START, SERIAL_BUTTON_SELECT
+from gamelib.data_helper_functions import load_image, load_font
 from gamelib.gamestate import GameState
 from gamelib.menus.menuscreen import MenuScreen
 from gamelib.uielements import UiText, ButtonIcon
@@ -13,16 +14,26 @@ class StartMenuScreen(MenuScreen):
 
         normal_game_text = UiText(self.sprites)
         normal_game_text.text = "Normales Spiel starten"
-        normal_game_text.targetRect = Rect(200, 120, 300, 300)
-        normal_game_text.font = pg.font.Font(None, 84)
-
+        normal_game_text.targetRect = Rect(160, 110, 300, 300)
+        normal_game_text.font = load_font(64)
         easy_game_text = UiText(self.sprites)
         easy_game_text.text = "Einfaches Spiel starten"
-        easy_game_text.targetRect = Rect(200, 260, 300, 300)
-        easy_game_text.font = pg.font.Font(None, 84)
-
+        easy_game_text.targetRect = Rect(160, 210, 300, 300)
+        easy_game_text.font = load_font(64)
         # noinspection PyTypeChecker
-        ButtonIcon(90, 95, [load_image(im, "buttons96") for im in ("button_black.png", "button_black_pressed.png")], self.sprites).frame = 12
-        ButtonIcon(90, 235, [load_image(im, "buttons96") for im in ("button_white.png", "button_white_pressed.png")], self.sprites)
+        ButtonIcon(50, 85, [load_image(im, "buttons96") for im in ("button_black.png", "button_black_pressed.png")], self.sprites).frame = 12
+        ButtonIcon(50, 185, [load_image(im, "buttons96") for im in ("button_white.png", "button_white_pressed.png")], self.sprites)
 
         self.cursor.text = "" # no cursor
+
+    def handle_key(self, key):
+        if key == SERIAL_BUTTON_START:
+            self.gamestate.reset(6)
+            self._close_menu()
+            return True
+        elif key == SERIAL_BUTTON_SELECT:
+            self.gamestate.reset(3)
+            self._close_menu()
+            return True
+        else:
+            return MenuScreen.handle_key(self, key)
