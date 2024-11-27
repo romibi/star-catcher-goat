7bbb22c51bcee9cd65359c32e5619526505c540c
romibi <romibi@bluewin.ch>
2024-11-27 14:09:54 +0100

    Reduce amount of requests in led test script

--- request_test.py
+++ request_test.py
@@ -9,32 +9,43 @@ def get_random_color():
     r = lambda: random.randint(0,255)
     return '%02X%02X%02X' % (r(),r(),r())
 
-def get_random_url(hub, segment):
+def get_post(hub):
     color = get_random_color()
     # todo use json api to use 1 request per hub
-    return f"http://10.128.0.10{hub}/win&SM={segment}&SB=255&CL=H{color}&SV=2"
+    # return f"http://10.128.0.10{hub}/win&SM={segment}&SB=255&CL=H{color}&SV=2"
+    url = f"http://10.128.0.10{hub}/json"
+    data = "{'seg':["
+    for segment in (0,1,2,3,4,5):
+        if segment != 0:
+            data += ","
+        color = get_random_color()
+        data += f"{{'id':{segment},'on':true, 'bri': 255, 'col': ['{color}']}}"
+    data += "]}"
+    return url, data
+
 
 GET_TIMEOUT = Timeout(timeout=5)
 
 # change color of segment 1 (0) to green: http://192.168.1.107/win&SM=0&SB=255&CL=H00FF00
 def update_leds():
-    urls = []
+    posts = []
 
     for hub in (1,2,3,4,5,6,7,8,9):
-        for segment in (0,1,2,3,4,5):
-            urls += [get_random_url(hub, segment)]
+        posts += [get_post(hub)]
 
-    def request_url(url, exception_handler):
+    def do_post(post, exception_handler):
         try:
-            resp = httpx.get(url, timeout=GET_TIMEOUT)
+            print(f"posting to {post[0]}: {post[1]}")
+            resp = httpx.post(post[0], data=post[1], timeout=GET_TIMEOUT)
             resp.close()
-        except:
+        except Exception as e:
+            print(e)
             if exception_handler:
                 exception_handler()
 
-    for url in urls:
+    for post in posts:
         # todo: use task groups
-        threading.Thread(target=request_url, args=(url,None)).start()
+        threading.Thread(target=do_post, args=(post,None)).start()
 
 def main():
     while True:
