e15c0164b4833fb8607360a9fea93f4d72a1a380
romibi <romibi@bluewin.ch>
2024-11-27 11:47:39 +0100

    Update request test code to not use grequests

--- request_test.py
+++ request_test.py
@@ -1,9 +1,9 @@
 import time
 import threading
-
-import grequests
-
 import random
+import httpx
+from httpx import Timeout
+
 
 def get_random_color():
     r = lambda: random.randint(0,255)
@@ -11,7 +11,10 @@ def get_random_color():
 
 def get_random_url(hub, segment):
     color = get_random_color()
+    # todo use json api to use 1 request per hub
     return f"http://10.128.0.10{hub}/win&SM={segment}&SB=255&CL=H{color}&SV=2"
+
+GET_TIMEOUT = Timeout(timeout=5)
 
 # change color of segment 1 (0) to green: http://192.168.1.107/win&SM=0&SB=255&CL=H00FF00
 def update_leds():
@@ -21,13 +24,17 @@ def update_leds():
         for segment in (0,1,2,3,4,5):
             urls += [get_random_url(hub, segment)]
 
-    rs = (grequests.get(u, timeout=5) for u in urls)
+    def request_url(url, exception_handler):
+        try:
+            resp = httpx.get(url, timeout=GET_TIMEOUT)
+            resp.close()
+        except:
+            if exception_handler:
+                exception_handler()
 
-    def call_map(rs, exception_handler):
-        grequests.map(rs, exception_handler=exception_handler, gtimeout=20)
-
-    threading.Thread(target=call_map, args=(rs,None)).start()
-    #call_map(rs,None)
+    for url in urls:
+        # todo: use task groups
+        threading.Thread(target=request_url, args=(url,None)).start()
 
 def main():
     while True:
